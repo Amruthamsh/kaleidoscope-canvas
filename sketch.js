f52e75cc93bceff8dbd686e10dae52bc63fa55d2
Amruthamsh A <32568328+Amruthamsh@users.noreply.github.com>
2024-03-20 21:07:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ let chunks = [];
 const fr = 30;
 let canvasW, canvasH;
 function setup() {
-  canvasH = windowHeight * 0.70;
+  canvasH = windowHeight * 0.75;
   canvasW = canvasH;
 
   h1 = createElement("h1", "Kaleidoscope Canvas");
